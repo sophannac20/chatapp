13bf67bc6b2eb66ca86d2ff7ebc2949b6b706e1f
Sophanna <user@Sophanna.local>
2025-06-07 14:30:06 +0700

    fix lower case

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -40,7 +40,7 @@ const Login = () => {
                       type="text"
                       placeholder="email"
                       value={email}
-                      onChange={(e) => setEmail(e.target.value.toLowerCase)}
+                      onChange={(e) => setEmail(e.target.value.toLowerCase())}
                       required
                     />
                   </Form.Group>
